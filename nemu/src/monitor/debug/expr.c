f6fb14d0bf514b0498c6c63f0f7805b3be26f985
tracer-NEMU2020 <tjuics2020@163.com>
2025-09-10 12:02:53 +0000

    >  compile NEMU 3023206114 chisa Linux 97ebd28820b2 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 16:52:50 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:02:53 up  6:37,  0 users,  load average: 0.00, 0.00, 0.00 dfe4d2ea2fd2b02fa024ac54342f9750b1208f69

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -1,31 +1,34 @@
 #include "nemu.h"
+
+/* We use the POSIX regex functions to process regular expressions.
+ * Type 'man regex' for more information about POSIX regex functions.
+ */
 #include <sys/types.h>
 #include <regex.h>
-#include <string.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <assert.h>
+#include <stdlib.h>   // for atoi
 
 enum {
-  NOTYPE = 256, EQ, NUM,
-  // 其他自定义 token_type 可以继续往下加
+  NOTYPE = 256, EQ, NUM, LEFT, RIGHT
+
+  /* TODO: Add more token types */
 };
 
 static struct rule {
   char *regex;
   int token_type;
 } rules[] = {
-  {" +",    NOTYPE},    // 空格
-  {"\\+",   '+'},       // 加号
-  {"-[0-9]+", NUM},     // 负数整体
-  {"-",     '-'},       // 减号
-  {"\\*",   '*'},       // 乘号
-  {"/",     '/'},       // 除号
-  {"[0-9]+", NUM},      // 正整数
-  // 以后你要加括号也可以: {"\\(", '('}, {"\\)", ')'},
+  {"[0-9]+", NUM},          // 数字
+  {" +",  NOTYPE},          // 空格
+  {"\\+", '+'},             // +
+  {"\\*", '*'},             // *
+  {"-", '-'},               // -
+  {"/", '/'},               // /
+  {"\\(", LEFT},            // (
+  {"\\)", RIGHT},           // )
+  {"==", EQ}                // ==
 };
 
-#define NR_REGEX (sizeof(rules) / sizeof(rules[0]))
+#define NR_REGEX (sizeof(rules) / sizeof(rules[0]) )
 
 static regex_t re[NR_REGEX];
 
@@ -34,11 +37,11 @@ void init_regex() {
   char error_msg[128];
   int ret;
 
-  for (i = 0; i < NR_REGEX; i ++) {
+  for(i = 0; i < NR_REGEX; i ++) {
     ret = regcomp(&re[i], rules[i].regex, REG_EXTENDED);
-    if (ret != 0) {
+    if(ret != 0) {
       regerror(ret, &re[i], error_msg, 128);
-      panic("regex compilation failed: %s\n%s", error_msg, rules[i].regex);
+      Assert(ret == 0, "regex compilation failed: %s\n%s", error_msg, rules[i].regex);
     }
   }
 }
@@ -48,46 +51,49 @@ typedef struct token {
   char str[32];
 } Token;
 
-static Token tokens[32];
-static int nr_token;
+Token tokens[32];
+int nr_token;
 
 static bool make_token(char *e) {
   int position = 0;
+  int i;
   regmatch_t pmatch;
 
   nr_token = 0;
 
-  while (e[position] != '\0') {
-    int i;
-    for (i = 0; i < NR_REGEX; i ++) {
-      if (regexec(&re[i], e + position, 1, &pmatch, 0) == 0 &&
-          pmatch.rm_so == 0) {
+  while(e[position] != '\0') {
+    for(i = 0; i < NR_REGEX; i ++) {
+      if(regexec(&re[i], e + position, 1, &pmatch, 0) == 0 && pmatch.rm_so == 0) {
         char *substr_start = e + position;
         int substr_len = pmatch.rm_eo;
 
+        Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s",
+          i, rules[i].regex, position, substr_len, substr_len, substr_start);
         position += substr_len;
 
-        if (rules[i].token_type == NOTYPE) {
-          break; // 空格直接跳过
-        }
-
-        tokens[nr_token].type = rules[i].token_type;
-        if (rules[i].token_type == NUM) {
-          if (substr_len >= sizeof(tokens[nr_token].str)) {
-            panic("number too long");
+        switch(rules[i].token_type) {
+          case NUM: {
+            tokens[nr_token].type = NUM;
+            strncpy(tokens[nr_token].str, substr_start, substr_len);
+            tokens[nr_token].str[substr_len] = '\0';
+            nr_token ++;
+            break;
           }
-          strncpy(tokens[nr_token].str, substr_start, substr_len);
-          tokens[nr_token].str[substr_len] = '\0';
-        } else {
-          tokens[nr_token].str[0] = '\0';
+          case NOTYPE: break;
+          default: {
+            tokens[nr_token].type = rules[i].token_type;
+            tokens[nr_token].str[0] = '\0';
+            nr_token ++;
+            break;
+          }
         }
-        nr_token ++;
         break;
       }
     }
 
-    if (i == NR_REGEX) {
-      printf("no match at position %d\n%s\n%*.s^\n", position, e, position, "");
+    if(i == NR_REGEX) {
+      printf("no match at position %d\n%s\n%*.s^\n",
+        position, e, position, "");
       return false;
     }
   }
@@ -95,86 +101,92 @@ static bool make_token(char *e) {
   return true;
 }
 
-static int eval(int p, int q, bool *success) {
+/* 辅助函数：括号检查 */
+static bool check_parentheses(int p, int q) {
+  if (tokens[p].type != LEFT || tokens[q].type != RIGHT) return false;
+
+  int balance = 0;
+  int i;
+  for (i = p; i <= q; i++) {
+    if (tokens[i].type == LEFT) balance++;
+    else if (tokens[i].type == RIGHT) balance--;
+
+    if (balance == 0 && i < q) return false;
+  }
+  return balance == 0;
+}
+
+/* 查找主操作符 */
+static int find_main_operator(int p, int q) {
+  int i;
+  int pos = -1;
+  int min_pri = 100;
+  int balance = 0;
+
+  for (i = p; i <= q; i++) {
+    if (tokens[i].type == LEFT) { balance++; continue; }
+    if (tokens[i].type == RIGHT) { balance--; continue; }
+    if (balance > 0) continue;
+
+    int pri = 10;
+    switch(tokens[i].type) {
+      case EQ: pri = 1; break;
+      case '+': case '-': pri = 2; break;
+      case '*': case '/': pri = 3; break;
+      default: pri = 100; break;
+    }
+
+    if (pri <= min_pri) {
+      min_pri = pri;
+      pos = i;
+    }
+  }
+  return pos;
+}
+
+/* 表达式求值 */
+static uint32_t eval(int p, int q, bool *success) {
   if (p > q) {
     *success = false;
     return 0;
   }
   else if (p == q) {
-    // 单个 token 必须是 NUM
-    if (tokens[p].type == NUM) {
-      int val;
-      sscanf(tokens[p].str, "%d", &val);
-      return val;
-    } else {
+    if (tokens[p].type != NUM) {
       *success = false;
       return 0;
     }
+    return atoi(tokens[p].str);
   }
-  else if (tokens[p].type == '(' && tokens[q].type == ')') {
-    // 被括号包裹的子表达式
+  else if (check_parentheses(p, q)) {
     return eval(p + 1, q - 1, success);
   }
-
-  // 查找主运算符（优先级最低）
-  int op = -1;
-  int min_pri = 10;
-  int i, level = 0;
-
-  for (i = p; i <= q; i++) {
-    if (tokens[i].type == '(') {
-      level++;
-      continue;
+  else {
+    int op = find_main_operator(p, q);
+    if (op == -1) {
+      *success = false;
+      return 0;
     }
-    if (tokens[i].type == ')') {
-      level--;
-      continue;
-    }
-    if (level > 0) continue; // 括号内跳过
 
-    int pri = 10;
-    if (tokens[i].type == '+' || tokens[i].type == '-') pri = 1;
-    else if (tokens[i].type == '*' || tokens[i].type == '/') pri = 2;
+    uint32_t val1 = eval(p, op - 1, success);
+    uint32_t val2 = eval(op + 1, q, success);
+    if (!*success) return 0;
 
-    if (pri <= min_pri) {
-      min_pri = pri;
-      op = i;
+    switch(tokens[op].type) {
+      case '+': return val1 + val2;
+      case '-': return val1 - val2;
+      case '*': return val1 * val2;
+      case '/': return val2 ? val1 / val2 : 0;
+      case EQ:  return val1 == val2;
+      default: *success = false; return 0;
     }
   }
-
-  if (op == -1) {
-    *success = false;
-    return 0;
-  }
-
-  bool success1 = true, success2 = true;
-  int val1 = eval(p, op - 1, &success1);
-  int val2 = eval(op + 1, q, &success2);
-  if (!success1 || !success2) {
-    *success = false;
-    return 0;
-  }
-
-  switch (tokens[op].type) {
-    case '+': return val1 + val2;
-    case '-': return val1 - val2;
-    case '*': return val1 * val2;
-    case '/': 
-      if (val2 == 0) {
-        *success = false;
-        return 0;
-      }
-      return val1 / val2;
-    default: assert(0);
-  }
-  return 0;
 }
 
-
-int expr(char *e, bool *success) {
-  if (!make_token(e)) {
+uint32_t expr(char *e, bool *success) {
+  if(!make_token(e)) {
     *success = false;
     return 0;
   }
+
   return eval(0, nr_token - 1, success);
 }
