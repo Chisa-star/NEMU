8478028449bda1b08195a973f8a185af5e7d8766
tracer-NEMU2020 <tjuics2020@163.com>
2025-09-11 07:41:25 +0000

    >  compile NEMU 3023206114 chisa Linux 97ebd28820b2 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 16:52:50 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  07:41:25 up 15:07,  0 users,  load average: 0.45, 0.22, 0.11 be3eb02bd8c412a77f0702da7be5e6d2d77116ea

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -241,13 +241,13 @@ static uint32_t eval(int p, int q, bool *success) {
     
     int op_pos = find_main_operator(p, q);
 
-    bool s1 = false, s2 = false;
-    uint32_t left = eval(p, op_pos - 1, &s1);
-    if (!s1) { *success = false; return 0; }
-    uint32_t right = eval(op_pos + 1, q, &s2);
-    if (!s2) { *success = false; return 0; }
     if (op_pos != -1)
     {
+        bool s1 = false, s2 = false;
+        uint32_t left = eval(p, op_pos - 1, &s1);
+        if (!s1) { *success = false; return 0; }
+        uint32_t right = eval(op_pos + 1, q, &s2);
+        if (!s2) { *success = false; return 0; }
         switch (tokens[op_pos].type) {
         case '+': *success = true; return left + right;
         case '-': *success = true; return left - right;
